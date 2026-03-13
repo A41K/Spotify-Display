9708df58586f5b12436dfe42b1df8b3d692cef61
A41K <arokandris@gmail.com>
2026-03-13 23:02:07 +0100

    1st commit

--- cODE/cODE.ino
+++ cODE/cODE.ino
@@ -1,74 +1,18 @@
-#include <Arduino.h>
-#include <ArduinoJson.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_ST7735.h>
-#include <WiFi.h>
-#include <SpotifyEsp32.h>
-#include <SPI.h>
-
-#define TFT_CS 1
-#define TFT_RST 2
-#define TFT_DC 3
-#define TFT_SCLK 4
-#define TFT_MOSI 5
-
-char* SSID = "YOUR WIFI SSID";
-const char* PASSWORD = "YOUR WIFI PASSWORD";
-const char* CLIENT_ID = "YOUR CLIENT ID FROM THE SPOTIFY DASHBOARD";
-const char* CLIENT_SECRET = "YOUR CLIENT SECRET FROM THE SPOTIFY DASHBOARD";
-
-String lastArtist;
-String lastTrackname;
-
-Spotify sp(CLIENT_ID, CLIENT_SECRET);
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
-
+const int buttonPin = 2;   // GPIO pin where the button is connected
 
 void setup() {
-    Serial.begin(115200);
-
-    tft.initR(INITR_BLACKTAB); // the type of screen
-    tft.setRotation(1); // this makes the screen landscape! remove this line for portrait
-    Serial.println("TFT Initialized!");
-    tft.fillScreen(ST77XX_BLACK); // make sure there is nothing in the buffer
-
-    WiFi.begin(SSID, PASSWORD);
-    Serial.print("Connecting to WiFi...");
-    while(WiFi.status() != WL_CONNECTED)
-    {
-        delay(1000);
-        Serial.print(".");
-    }
-    Serial.printf("\nConnected!\n");
-
-    tft.setCursor(0,0); // make the cursor at the top left
-    tft.write(WiFi.localIP().toString().c_str()); // print out IP on the screen
-
-    sp.begin();
-    while(!sp.is_auth()){
-        sp.handle_client();
-    }
-    Serial.println("Authenticated");
+  Serial.begin(115200);
+  pinMode(buttonPin, INPUT_PULLUP); // enable internal pull-up
 }
 
-void loop()
-{
-    String currentArtist = sp.current_artist_names();
-    String currentTrackname = sp.current_track_name();
+void loop() {
+  int buttonState = digitalRead(buttonPin);
 
-    if (lastArtist != currentArtist && currentArtist != "Something went wrong" && !currentArtist.isEmpty()) {
-        tft.fillScreen(ST77XX_BLACK);
-        lastArtist = currentArtist;
-        Serial.println("Artist: " + lastArtist);
-        tft.setCursor(10,10);
-        tft.write(lastArtist.c_str());
-    }
+  if (buttonState == LOW) {  // pressed
+    Serial.println("Button pressed");
+  } else {
+    Serial.println("Button released");
+  }
 
-    if (lastTrackname != currentTrackname && currentTrackname != "Something went wrong" && currentTrackname != "null") {
-        lastTrackname = currentTrackname;
-        Serial.println("Track: " + lastTrackname);
-        tft.setCursor(10,40);
-        tft.write(lastTrackname.c_str());
-    }
-    delay(2000);
+  delay(200);
 }
